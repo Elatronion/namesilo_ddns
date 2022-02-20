a07dc0ee61f50d1cb49ea851b6f677b31ebc5e4b
Ezra Hradecky <ezra@duck.com>
2022-02-19 20:51:08 -0500

    update namesilo_ddns.sh to be more generic
    The file can now be executed with various parameters;
    in order: APIKEY, TYPE, DOMAIN, HOST (optional)

--- namesilo_ddns.sh
+++ namesilo_ddns.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-##Domain name:
-DOMAIN="mydomain.tld"
-
-##Host name (subdomain). Optional. If present, must end with a dot (.)
-HOST="subdomain."
-
 ##APIKEY obtained from Namesilo:
-APIKEY="c40031261ee449037a4b4"
+APIKEY=$1
+##TYPE (A, TXT)
+TYPE=$2
+##Domain name (example.com)
+DOMAIN=$3
+##Host name / subdomain (www). Optional.
+HOST=$4
+
+if [ -z $HOST ]
+then
+	URL=$DOMAIN
+else
+	URL=$HOST.$DOMAIN
+fi
 
 ## Do not edit lines below ##
 
 ##Saved history pubic IP from last check
-IP_FILE="/var/tmp/MyPubIP"
+IP_FILE="/var/tmp/MyPubIP_$HOST$DOMAIN"
 
 ##Time IP last updated or 'No IP change' log message output
-IP_TIME="/var/tmp/MyIPTime"
+IP_TIME="/var/tmp/MyIPTime_$HOST$DOMAIN"
 
 ##How often to output 'No IP change' log messages
 NO_IP_CHANGE_TIME=86400
 
 ##Response from Namesilo
-RESPONSE="/tmp/namesilo_response.xml"
+RESPONSE="/tmp/namesilo_response_$HOST$DOMAIN.xml"
 
 ##Choose randomly which OpenDNS resolver to use
 RESOLVER=resolver$(echo $((($RANDOM%4)+1))).opendns.com
@@ -58,15 +65,28 @@ if [ "$CUR_IP" != "$KNOWN_IP" ]; then
   echo $CUR_IP > $IP_FILE
   logger -t IP.Check -- Public IP changed to $CUR_IP from $RESOLVER
 
-  ##Update DNS record in Namesilo:
+  ## Update DNS record in Namesilo:
   curl -s "https://www.namesilo.com/api/dnsListRecords?version=1&type=xml&key=$APIKEY&domain=$DOMAIN" > $DOMAIN.xml 
-  RECORD_ID=`xmllint --xpath "//namesilo/reply/resource_record/record_id[../host/text() = '$HOST$DOMAIN' ]" $DOMAIN.xml | grep -oP '(?<=<record_id>).*?(?=</record_id>)'`
-  curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrid=$RECORD_ID&rrhost=$HOST&rrvalue=$CUR_IP&rrttl=3600" > $RESPONSE
-    RESPONSE_CODE=`xmllint --xpath "//namesilo/reply/code/text()"  $RESPONSE`
+
+  ## Only get first record, (fixes crash if multiple records exist)
+  RECORD_ID=`xmllint --xpath "//namesilo/reply/resource_record/record_id[../host/text() = '$URL' ]" $DOMAIN.xml | grep -oP '(?<=<record_id>).*?(?=</record_id>)'`
+  stringarray=($RECORD_ID)
+  RECORD_ID=${stringarray[0]} ## TODO: Allow for multiple records to be considered
+
+  ## RECORD_ID is equaled to every domain dns with subdomain, sepereated by a space, only works with one now.
+if [ "$TYPE" == "A" ]
+then
+	curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrid=$RECORD_ID&rrhost=$HOST&rrvalue=$CUR_IP&rrttl=3600" > $RESPONSE
+elif [ "$TYPE" == "TXT" ]
+then	
+	curl -s "https://www.namesilo.com/api/dnsUpdateRecord?version=1&type=xml&key=$APIKEY&domain=$DOMAIN&rrid=$RECORD_ID&rrhost=$HOST&rrvalue=v=spf1%20a%20mx%20ptr%20ip4:$CUR_IP%20~all&rrttl=3600" > $RESPONSE
+fi
+
+	RESPONSE_CODE=`xmllint --xpath "//namesilo/reply/code/text()"  $RESPONSE`
        case $RESPONSE_CODE in
        300)
          date "+%s" > $IP_TIME
-         logger -t IP.Check -- Update success. Now $HOST$DOMAIN IP address is $CUR_IP;;
+         logger -t IP.Check -- Update success. Now $URL IP address is $CUR_IP;;
        280)
          logger -t IP.Check -- Duplicate record exists. No update necessary;;
        *)
